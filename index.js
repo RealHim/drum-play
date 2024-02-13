2bab61d96ac49cffc5d7e8db689018762c11fa2d
Himanshu <136063917+RealHim@users.noreply.github.com>
2024-02-13 19:34:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+//For detecting clicking on button
+for(var i = 0; i<document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", (function() {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        addAnimation(buttonInnerHTML);
+    }));
+}
+
+//For detecting key press
+document.addEventListener("keydown", (function(event) {
+    makeSound(event.key);
+    addAnimation(event.key);
+}));
+
+//Selecting volume slider
+const volumeSlider = document.querySelector(".volume-slider input");
+
+//For playing sounds when clicked buttons or pressed by keyboard
+function makeSound(key) {
+    var volume = volumeSlider.value;
+    switch (key) {
+        case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.volume = volume;
+                tom1.play();   
+            break;
+        case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.volume = volume;
+                tom2.play();
+            break;
+        case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.volume = volume;
+                tom3.play();
+            break;
+        case "d":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.volume = volume;
+                tom4.play();
+            break;
+        case "j":
+                var snare = new Audio('sounds/snare.mp3');
+                snare.volume = volume;
+                snare.play();
+            break;
+        case "k":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.volume = volume;
+                crash.play();
+            break;
+        case "l":
+                var kick = new Audio('sounds/kick-bass.mp3');
+                kick.volume = volume;
+                kick.play();
+            break;
+    
+        default:
+                console.log("You pressed invalid button");
+            break;
+    }
+}
+
+//For animations when button gets clicked or pressed
+function addAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout((function() {
+    activeButton.classList.remove("pressed");
+  }), 100);
+}
+
+
+
+
